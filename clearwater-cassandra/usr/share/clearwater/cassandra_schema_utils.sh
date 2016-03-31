26c53e22e2f7187086c2c69e347341494d987640
Graeme Robertson <graeme.robertson@metaswitch.com>
2016-03-31 15:34:09 +0000

    Allow configurable replication facotr

--- clearwater-cassandra/usr/share/clearwater/cassandra_schema_utils.sh
+++ clearwater-cassandra/usr/share/clearwater/cassandra_schema_utils.sh
@@ -34,9 +34,13 @@
 
 . /etc/clearwater/config
 
+# The default replication factor is 2, but the calling script may have other
+# ideas.
+replication_factor=${replication_factor:-2}
+
 # Create the common REPLICATION string to be used when creating Clearwater
 # Cassandra schemas. This file gets dotted in by the schema creation scripts.
-replication_str="{'class': 'SimpleStrategy', 'replication_factor': 2}"
+replication_str="{'class': 'SimpleStrategy', 'replication_factor': $replication_factor}"
 
 # If local_site_name and remote_site_names are set then this is a GR
 # deployment. Set the replication strategy to NetworkTopologyStrategy and
@@ -44,11 +48,10 @@ replication_str="{'class': 'SimpleStrategy', 'replication_factor': 2}"
 if [ -n "$local_site_name" ] && [ -n "$remote_site_names" ]
 then
   IFS=',' read -a remote_site_names_array <<< "$remote_site_names"
-  replication_str="{'class': 'NetworkTopologyStrategy', '$local_site_name': 2"
+  replication_str="{'class': 'NetworkTopologyStrategy', '$local_site_name': $replication_factor"
   for remote_site in "${remote_site_names_array[@]}"
   do
-    # Set the replication factor for each site to 2.
-    replication_str+=", '$remote_site': 2"
+    replication_str+=", '$remote_site': $replication_factor"
   done
   replication_str+="}"
 fi
