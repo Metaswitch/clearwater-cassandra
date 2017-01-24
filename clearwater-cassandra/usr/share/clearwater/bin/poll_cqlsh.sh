5fb13d53f291f79579617b6abaca74061de7e5fa
Eleanor Merry <eleanor.merry@metaswitch.com>
2017-01-24 17:12:52 +0000

    Add more cassandra checks

---
+++ clearwater-cassandra/usr/share/clearwater/bin/poll_cqlsh.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# @file poll_cqlsh.sh
+#
+# Project Clearwater - IMS in the Cloud
+# Copyright (C) 2017  Metaswitch Networks Ltd
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the "Special Exception" for use of
+# the program along with SSL, set forth below. This program is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details. You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# The author can be reached by email at clearwater@metaswitch.com or by
+# post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+#
+# Special Exception
+# Metaswitch Networks Ltd  grants you permission to copy, modify,
+# propagate, and distribute a work formed by combining OpenSSL with The
+# Software, or a work derivative of such a combination, even if such
+# copying, modification, propagation, or distribution would otherwise
+# violate the terms of the GPL. You must comply with the GPL in all
+# respects for all of the code used other than OpenSSL.
+# "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+# Project and licensed under the OpenSSL Licenses, or a work based on such
+# software and licensed under the OpenSSL Licenses.
+# "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+# under which the OpenSSL Project distributes the OpenSSL toolkit software,
+# as those licenses appear in the file LICENSE-OPENSSL.
+
+. /usr/share/clearwater/utils/check-root-permissions 1
+
+[ $# -le 1 ] || { echo "Usage: poll_cqlsh [--no-grace-period] (defaults to a two minute grace period)" >&2 ; exit 2 ; }
+
+# Get how long the cassandra process has been running. If this is less than
+# 2 minutes, then don't poll cqlsh (as it may not be up yet), unless
+# we're specifically asked to.
+# Getting the uptime can fail if the cassandra process fails - this is caught
+# by the monit script.
+if [ -z "$1" ]; then
+  value=$( ps -p $( cat /var/run/cassandra/cassandra.pid ) -o etimes=)
+  if [ $? == 0 ] && [ "$value" -lt 120 ]; then
+    exit 0
+  fi
+fi
+
+# This script attempts to connect to cassandra over cqlsh. This script should be
+# run in the signaling namespace if set.
+/usr/share/clearwater/bin/run-in-signaling-namespace cqlsh -e quit
+rc=$?
+exit $rc
